3aff562c80d375b5fa5109164aadd1297adfa458
Becca <becca.vandewater@gmail.com>
2023-07-25 21:55:52 -0600

    Events in back end complete!

--- Tower-vande/server/controllers/TowerEventsController.js
+++ Tower-vande/server/controllers/TowerEventsController.js
@@ -7,9 +7,35 @@ export class TowerEventsController extends BaseController {
     super('api/events')
 
     this.router
+      .get('', this.getAllEvents)
+      .get('/:eventId', this.getEventById)
+
       .use(Auth0Provider.getAuthorizedUserInfo)
 
       .post('', this.createEvent)
+      .put('/:eventId', this.editEvent)
+      .delete('/:eventId', this.cancelEvent)
+  }
+  async getAllEvents(req, res, next) {
+    try {
+      const towerEvents = await towerEventsService.getAllEvents()
+
+      return res.send(towerEvents)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getEventById(req, res, next) {
+    try {
+      const eventId = req.params.eventId
+
+      const towerEvent = await towerEventsService.getEventById(eventId)
+
+      return res.send(towerEvent)
+    } catch (error) {
+      next(error)
+    }
   }
   async createEvent(req, res, next) {
     try {
@@ -20,6 +46,36 @@ export class TowerEventsController extends BaseController {
       const towerEvent = await towerEventsService.createEvent(eventData)
 
       return res.send(towerEvent)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editEvent(req, res, next) {
+    try {
+      const eventData = req.body
+
+      const eventId = req.params.eventId
+
+      const userId = req.userInfo.id
+
+      const editedEvent = await towerEventsService.editEvent(eventData, eventId, userId)
+
+      return res.send(editedEvent)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async cancelEvent(req, res, next) {
+    try {
+      const eventId = req.params.eventId
+
+      const userId = req.userInfo.id
+
+      const canceledEvent = await towerEventsService.cancelEvent(eventId, userId)
+
+      return res.send(canceledEvent)
     } catch (error) {
       next(error)
     }
--- Tower-vande/server/services/TowerEventsService.js
+++ Tower-vande/server/services/TowerEventsService.js
@@ -1,12 +1,62 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest, Forbidden } from "../utils/Errors.js"
 
 class TowerEventsService {
+  async getAllEvents() {
+    const towerEvents = await dbContext.TowerEvents.find().populate('ticketCount')
+
+    return towerEvents
+  }
+
+  async getEventById(eventId) {
+    const towerEvent = (await dbContext.TowerEvents.findById(eventId)).populate('ticketCount')
+
+    return towerEvent
+  }
   async createEvent(eventData) {
     const towerEvent = await dbContext.TowerEvents.create(eventData)
 
     await towerEvent.populate('ticketCount')
 
     return towerEvent
+  }
+
+  async editEvent(eventData, eventId, userId) {
+    const originalEvent = await this.getEventById(eventId)
+
+    if (originalEvent.creatorId != userId) {
+      throw new Forbidden(`You are not the owner of ${originalEvent.name}. You may not edit this event.`)
+    }
+
+    if (originalEvent.isCanceled == true) {
+      throw new BadRequest(`${originalEvent.name} has been canceled. You may not edit this event.`)
+    }
+
+    originalEvent.name = eventData.name || originalEvent.name
+    originalEvent.description = eventData.description || originalEvent.description
+    originalEvent.coverImg = eventData.coverImg || originalEvent.coverImg
+    originalEvent.location = eventData.location || originalEvent.location
+    originalEvent.capacity = eventData.capacity || originalEvent.capacity
+    originalEvent.startDate = eventData.startDate || originalEvent.startDate
+    originalEvent.type = eventData.type || originalEvent.type
+
+    await originalEvent.save()
+
+    return originalEvent
+  }
+
+  async cancelEvent(eventId, userId) {
+    const eventToCancel = await this.getEventById(eventId)
+
+    if (eventToCancel.creatorId != userId) {
+      throw new Forbidden(`You are not the owner of ${eventToCancel.name}. You may not cancel this event.`)
+    }
+
+    eventToCancel.isCanceled = true
+
+    await eventToCancel.save()
+
+    return eventToCancel
   }
 
 }
